7ff5c6fcd2b36d593ec53fe1eb1036633ac6e371
Fernanda Lavalle <fernanda.lavalle@radiopublic.com>
2018-06-24 19:22:40 -0500

    added tests and limited what lines the linter looks at

---
+++ tests/data/test_file_for_lint.py
@@ -0,0 +1,23 @@
+import foo
+
+
+class TestClass(object):
+
+    # This line should throw an error because it is a comment and exceeds the maximum line length.
+    def __init__(self):
+        """This line should throw an error because it is over 80 characters and within a docstring."""
+        self._string_too_long = "This string is too long, it exceeds the character limit."
+
+    def this_function_name_should_not_throw_an_error_because_we_are_only_checking_comments_and_strings(
+            self):
+        """This line should not throw an error because it contains a URL: https://ketohub.io"""
+        an_array = []
+        for something in ['another', 'array']:
+            # This comment contains a URL, so it can be over the character limit: https://ketohub.io
+            an_array.append(something)
+        """
+        A triple-quote string can be over the limit whether or not it contains a URL.
+        """
+
+        """A triple-quote string can be over the limit when it's on three lines or just one."""
+        return another_array
